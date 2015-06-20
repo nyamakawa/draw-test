85f80b52f1da881ab5ea83265990a880bcf90b67
nyamakawa2 <nyamakawa2@gmail.com>
2015-06-20 16:04:20 +0900

    careate pixel draw function

--- src/draw_test/core.clj
+++ src/draw_test/core.clj
@@ -19,18 +19,27 @@
 ;; gets the pixels of the image, as an int array
 (def pixels  (get-pixels bi))
 
+(defn set-rgb [buf x y r g b]
+  (let [color (rgb r g b)]
+    (aset buf (+ x (* y size)) color)))
+
 (defn demo []
 
   ;; fill some random pixels with colours
   (dotimes [i (* size size)]
-    (aset pixels i (rand-colour)))
+;;    (aset pixels i (rand-colour)))
+    (aset pixels i (rgb 0 1 0)))
+
+  (dotimes [i 200]
+    (dotimes [j 200]
+      (set-rgb  pixels (+ 200 i) (+ j 200) 1 0 0)))
 
   ;; update the image with the newly changed pixel values
   (set-pixels bi pixels)
 
   ;; view our new work of art
   ;; the zoom function will automatically interpolate the pixel values
-  (show bi :zoom 1.0 :title "Isn't it beautiful?"))
+  (show bi :zoom 1.0 :title ""))
 
 (defn -main [& args]
   ;; show the iamge, after applying an "invert" filter
